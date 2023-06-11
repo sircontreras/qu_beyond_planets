eb444c3124427ebec99a9018a94d4ba4e1b7bd75
Rainy Contreras <rainy.contreras@gmail.com>
2023-06-10 20:31:27 -0400

    Routes and BgStart component created
    Test

---
+++ src/components/StarsBgWrapper.tsx
@@ -0,0 +1,34 @@
+import React, {PropsWithChildren} from "react";
+import {Box} from "@mui/material";
+import bgStars from "../images/bgStars.png";
+
+const StarsBgWrapper: React.FC<PropsWithChildren> = ({children})=>{
+
+    return (
+        <Box sx={{
+            width:'100vw',
+            height: '100vh',
+            background: `linear-gradient(175deg, rgba(0,0,0,1) 0%, rgba(2,2,44,1) 100%)`,
+            padding: '25px',
+            '&:before' : {
+                content: '""',
+                background: `url(${bgStars})`,
+                backgroundSize: '1584px 755px',
+                backgroundRepeat : 'repeat',
+                width: '100vw',
+                height: '100vh',
+                display: 'block',
+                opacity: 0.3,
+                position : 'absolute',
+                top: 0,
+                left: 0,
+
+            }
+        }}>
+            {children}
+        </Box>
+    )
+
+}
+
+export default StarsBgWrapper;
---
+++ src/pages/PlanetListPage.tsx
@@ -0,0 +1,15 @@
+import React, {PropsWithChildren} from "react";
+import StarsBgWrapper from "../components/StarsBgWrapper";
+
+
+const PlanetListPage:React.FC<PropsWithChildren> = ({children})=>{
+
+    return (
+        <StarsBgWrapper>
+            test
+        </StarsBgWrapper>
+    )
+
+}
+
+export default PlanetListPage;
